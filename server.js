3681cb79f6b886660de570e2a94f905be86ac3f8
Robert Albenda <ralbenda@gmail.com>
2022-02-12 12:24:45 -0500

    phase 1 done

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const sequelize = new Sequelize(
   process.env.DATABSE_URL || "postgres://localhost/bookmarks_plus"
 );
 
-const Bookmarks = sequelize.define('bookmark', {
+const Bookmarks = sequelize.define("bookmark", {
   name: {
     type: Sequelize.DataTypes.STRING,
     allowNull: false,
   },
 });
 
-const Categories = sequelize.define('category', {
-    name: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull:false,
-    },
+const Categories = sequelize.define("category", {
+  name: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
 Bookmarks.belongsTo(Categories);
@@ -22,20 +22,26 @@ Categories.hasMany(Bookmarks);
 
 /* express app*/
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => res.redirect('/bookmarks'))
-app.get('/bookmarks', async(req,res,next) => {
-    const bookmarks = await Bookmarks.findAll( {include:[Categories] });
-    const categories = await Categories.findByPk((req.params.id), {include:[Bookmarks]});
-    const html = bookmarks.map( bookmark => `
-    <div>
-    ${bookmark.name} <a href = '/categories/categoryName'> ${categories}</a>
-    </div>
-    `).join('')
-    res.send(`
+app.get("/", (req, res) => res.redirect("/bookmarks"));
+app.get("/bookmarks", async (req, res, next) => {
+  const bookmarks = await Bookmarks.findAll({ include: [Categories] });
+
+  const html = bookmarks
+    .map((bookmark) => {
+      const div = `
+        <div>
+        ${bookmark.name} <a href = '/categories/categoryName'> ${bookmark.category.name} </a>
+        </div>
+        `;
+      return div;
+    })
+    .join("");
+
+  res.send(`
     <html>
         <head>
             <title> ACME Bookmarks Page</title>
@@ -45,32 +51,30 @@ app.get('/bookmarks', async(req,res,next) => {
             </ul>
         </head>
     </html>
-    `)
-})
+    `);
+});
 
 const start = async () => {
   try {
-    console.log('hello world');
-    
+    console.log("hello world");
+
     app.listen(port, () => {
-        console.log(`listening on port ${port}`)
+      console.log(`listening on port ${port}`);
     });
 
     await sequelize.sync({ force: true });
-    await Bookmarks.create({ name: "Google.com" });
-    await Bookmarks.create({ name: "Indeed.com" });
-    await Bookmarks.create({ name: "stackoverflow.com" });
-    await Bookmarks.create({ name: "linkedIn.com" });
-    await Bookmarks.create({ name: "msdn.com" });
-    await Bookmarks.create({ name: "bing.com" });
-    await Categories.create({ name: 'Search'});
-    await Categories.create({ name: 'Coding'});ß
-    await Categories.create({ name: 'Jobs'});
-    console.log(Bookmarks);
+    const search = await Categories.create({ name: "Search" });
+    const coding = await Categories.create({ name: "Coding" });
+    const jobs = await Categories.create({ name: "Jobs" });
+    await Bookmarks.create({ name: "Google.com", categoryId: search.id });
+    await Bookmarks.create({ name: "Indeed.com", categoryId: coding.id });
+    await Bookmarks.create({ name: "stackoverflow.com", categoryId: jobs.id });
+    await Bookmarks.create({ name: "linkedIn.com", categoryId: jobs.id });
+    await Bookmarks.create({ name: "msdn.com", categoryId: coding.id });
+    await Bookmarks.create({ name: "bing.com", categoryId: search.id });
   } catch (ex) {
     console.log(ex);
   }
 };
 
 start();
-
