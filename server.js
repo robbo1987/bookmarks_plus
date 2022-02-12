28864f84357b4f8650a9eae45e16c7cc86eb2594
Robert Albenda <ralbenda@gmail.com>
2022-02-12 11:54:50 -0500

    almost got phase 1

--- server.js
+++ server.js
@@ -17,9 +17,45 @@ const Categories = sequelize.define('category', {
     },
 });
 
+Bookmarks.belongsTo(Categories);
+Categories.hasMany(Bookmarks);
+
+/* express app*/
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) => res.redirect('/bookmarks'))
+app.get('/bookmarks', async(req,res,next) => {
+    const bookmarks = await Bookmarks.findAll( {include:[Categories] });
+    const categories = await Categories.findByPk((req.params.id), {include:[Bookmarks]});
+    const html = bookmarks.map( bookmark => `
+    <div>
+    ${bookmark.name} <a href = '/categories/categoryName'> ${categories}</a>
+    </div>
+    `).join('')
+    res.send(`
+    <html>
+        <head>
+            <title> ACME Bookmarks Page</title>
+            <h1> ACME Bookmarks Page</h1>
+            <ul>
+                ${html}
+            </ul>
+        </head>
+    </html>
+    `)
+})
+
 const start = async () => {
   try {
-    console.log("hello world");
+    console.log('hello world');
+    
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`)
+    });
+
     await sequelize.sync({ force: true });
     await Bookmarks.create({ name: "Google.com" });
     await Bookmarks.create({ name: "Indeed.com" });
@@ -28,7 +64,7 @@ const start = async () => {
     await Bookmarks.create({ name: "msdn.com" });
     await Bookmarks.create({ name: "bing.com" });
     await Categories.create({ name: 'Search'});
-    await Categories.create({ name: 'Coding'});
+    await Categories.create({ name: 'Coding'});ß
     await Categories.create({ name: 'Jobs'});
     console.log(Bookmarks);
   } catch (ex) {
@@ -37,3 +73,4 @@ const start = async () => {
 };
 
 start();
+
